cc89e40198f41ccf9279338a1d9d57ba06ec1460
RetiredDairyCow <atg.ashesh@gmail.com>
2021-11-24 12:29:58 -0800

    added first test

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const blogsRouter = require('./controllers/blogs')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-
-
-
 app.use(express.json()) /*call this before request looger middleware or else it won't be able to parse JSON body*/
 app.use(middleware.requestLogger)
 app.use(cors())
---
+++ tests/list_test.test.js
@@ -0,0 +1,9 @@
+const listHelper = require('../utils/list_helper')
+
+test('dummy should return one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+}
+)
