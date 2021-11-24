6d546bf673c7a5333b2efeefba413722a9c99cd1
RetiredDairyCow <atg.ashesh@gmail.com>
2021-11-24 14:55:21 -0800

    exercise 4.5 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
-const dummy = (blogs) => {
-  return 1
+const favoriteBlog = (blogs) => {
+  const max = blogs.reduce((prev, current) => 
+    (prev.likes > current.likes) ? prev : current)
+  const {_id, url, __v, ...retObj} = max
+  return retObj
+}
+
+const totalLikes = (blogs) => {
+ return blogs.reduce((sum, obj) => {
+   return sum + obj.likes
+ }, 0)
 }
 
 module.exports = {
-  dummy
+  favoriteBlog, totalLikes
 }
