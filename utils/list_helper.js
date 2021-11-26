8b368fb20406a684c719ad4bf4400d23711a04ba
RetiredDairyCow <atg.ashesh@gmail.com>
2021-11-26 15:21:44 -0800

    exercise 4.7 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,53 @@
+const _ = require('lodash')
+
+const mostLikedAuthor = (blogsList) => {
+  const mapAuthorandLikes = new Map()
+  blogsList.forEach(({author, likes}) => {
+    if (!mapAuthorandLikes.has(author)) {
+      mapAuthorandLikes.set(author, likes)
+    }
+    else {
+      mapAuthorandLikes.set(author, mapAuthorandLikes.get(author) + likes)
+    }
+  })
+  const mostLikes = [...mapAuthorandLikes.entries()].reduce((acc, auth) => auth[1] > acc[1] ? auth : acc)
+  const retObj = {
+    author : mostLikes[0],
+    likes : mostLikes[1]
+  }
+  return retObj
+
+}
+
+const mostCommonAuthor = (blogsList) => {
+  const arr1 = blogsList.map(e => e.author)
+  
+  /**Function to calculate most common string in blogs author array */
+  var mf = 0 /*max freq*/
+  var m = 0 /*current max*/
+  var author
+  for (var i=0; i<arr1.length; i++)
+  {
+    for (var j=i; j<arr1.length; j++)
+    {
+      if (arr1[i] == arr1[j])
+        m++;
+      if (mf<m)
+      {
+        mf=m; 
+        author = arr1[i];
+      }
+    }
+    m=0;
+  }
+  const blogs = mf
+  const retObj = {
+    author,
+    blogs
+  }
+  return retObj
+}
+
 const favoriteBlog = (blogs) => {
   const max = blogs.reduce((prev, current) => 
     (prev.likes > current.likes) ? prev : current)
@@ -12,5 +62,8 @@ const totalLikes = (blogs) => {
 }
 
 module.exports = {
-  favoriteBlog, totalLikes
+  mostLikedAuthor,
+  mostCommonAuthor,
+  favoriteBlog,
+  totalLikes
 }
